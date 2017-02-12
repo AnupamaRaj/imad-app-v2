7ce0b57a268aa7f26a6bdc9e5391910c29d3c7f9
Anupama Raj <anu.anupamaraj@gmail.com>
2017-02-12 22:41:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var img=document.getElementById("image");
+var marginleft=0;
+function moveRight(){
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + "px";
+    
+}
+img.onclick()=function(){
+    var interval=setInterval(moveRight,50);
+};
